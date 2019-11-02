356ea7bc7776f54c78e0d3c4e6401b0ae81287bf
Jennifer Gomez <jennifergomezbruno@gmail.com>
2019-11-02 15:22:29 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -44,7 +44,9 @@ def naming_system
             label: "Family",
             sub_category: {
               label: "Genus"
-          
+                sub_category: {
+                  label: "Species"
+                }
               }
             }
           }
