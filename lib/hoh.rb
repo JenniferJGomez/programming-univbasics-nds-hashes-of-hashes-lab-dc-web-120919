34f65ed49ca6ea2873367dd66c2c41531859881c
Jennifer Gomez <jennifergomezbruno@gmail.com>
2019-11-02 15:06:50 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -33,13 +33,13 @@
 def naming_system
   
   hash ={
-    # PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
+    PORTION_1 = {
+  label: "Kingdom",
+  sub_category: {
+    label: "Phylum",
+    sub_category: {
+      label: "Class"
+    }
     }
   
 
